f94d63c259378fa033632dde738201300bd7ec7a
Alibubere <alibubere989@gmail.com>
2026-01-28 16:21:58 +0530

    Add optimized dataset loading function

---
+++ src/data_prep/optimized_dataset.py
@@ -0,0 +1,38 @@
+import os
+import logging
+from torch.utils.data import TensorDataset
+import torch
+from src.data_prep.Dataset import ImageDatasetwithCV
+
+def get_optimized_dataset(raw_dataset_path,save_path,mode="RGB",transform=None):
+
+    if os.path.exists(save_path):
+
+        logging.info(F"Loading processed data from {save_path}")
+
+        data = torch.load(save_path)
+
+        return TensorDataset(data["images"],data["labels"])
+    
+    else:
+        logging.info(f"No saved data found. processing with Opencv {mode}")
+
+        processed_set = ImageDatasetwithCV(root_dir=raw_dataset_path,mode=mode,transform=transform)
+
+        all_images = []
+        all_labels = []
+
+        for i in range(len(processed_set)):
+
+            img_tensor , label = processed_set[i]
+            all_images.append(img_tensor)
+            all_labels.append(label)
+
+        images_tensor = torch.stack(all_images)
+        labels_tensor = torch.stack(all_labels)
+
+        logging.info(f"Saving processed data to {save_path}")
+        torch.save({"images":images_tensor,"labels":labels_tensor},save_path)
+
+        return TensorDataset(images_tensor,labels_tensor)
+    
