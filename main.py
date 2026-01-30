8c958de1c7f784aed3113f865a87d3dc4672a61e
Alibubere <alibubere989@gmail.com>
2026-01-30 18:03:21 +0530

    training logic in main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from src.model.train_loop import train
 from src.model.train_utils import get_optimizer, get_resnet18_model
 from src.data_prep.dataset_split import get_split_data
 from src.data_prep.dataloader import get_dataloader
+import torch
 
 
 def setup_logging():
@@ -30,6 +31,8 @@ def setup_logging():
 def main():
 
     setup_logging()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     with open("configs/config.yaml", "r") as f:
         config = yaml.safe_load(f)
@@ -52,6 +55,35 @@ def main():
     lr = train_config["lr"]
     weight_decay = train_config["weight_decay"]
     resume = train_config["resume"]
+
+    # Model Config
+    model_config = config["model"]
+    model_dir = model_config["model_dir"]
+    os.makedirs(model_dir, exist_ok=True)
+
+    # Gray paths
+    gray_latest = model_config["gray_latest"]
+    gray_latest_full = os.path.join(model_dir, gray_latest)
+    gray_best = model_config["gray_best"]
+    gray_best_full = os.path.join(model_dir, gray_best)
+
+    # Clahe paths
+    clahe_latest = model_config["clahe_latest"]
+    clahe_latest_full = os.path.join(model_dir, clahe_latest)
+    clahe_best = model_config["clahe_best"]
+    clahe_best_full = os.path.join(model_dir, clahe_best)
+
+    # Edges paths
+    edges_latest = model_config["edges_latest"]
+    edges_latest_full = os.path.join(model_dir, edges_latest)
+    edges_best = model_config["edges_best"]
+    edges_best_full = os.path.join(model_dir, edges_best)
+
+    # RGB paths
+    rgb_latest = model_config["rgb_latest"]
+    rgb_latest_full = os.path.join(model_dir, rgb_latest)
+    rgb_best = model_config["rgb_best"]
+    rgb_best_full = os.path.join(model_dir, rgb_best)
 
     train_transform = get_train_transform()
     test_transform = get_test_transform()
@@ -150,6 +182,67 @@ def main():
         pin_memory=True,
         persistent_workers=True,
         prefetch_factor=True,
+    )
+
+    model = get_resnet18_model(device=device)
+    optimizer = get_optimizer(model=model, lr=lr, weight_decay=weight_decay)
+    scaler = torch.amp.GradScaler("cuda")
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    history_rgb = train(
+        resume=resume,
+        model=model,
+        optimizer=optimizer,
+        num_epochs=num_epochs,
+        device=device,
+        train_loader=train_loader_rgb,
+        test_loader=test_loader_rgb,
+        latest_path=rgb_latest_full,
+        best_path=rgb_best_full,
+        loss_fn=loss_fn,
+        scaler=scaler,
+    )
+
+    history_clahe = train(
+        resume=resume,
+        model=model,
+        optimizer=optimizer,
+        num_epochs=num_epochs,
+        device=device,
+        train_loader=train_loader_clahe,
+        test_loader=test_loader_clahe,
+        latest_path=clahe_latest_full,
+        best_path=clahe_best_full,
+        loss_fn=loss_fn,
+        scaler=scaler,
+    )
+
+    history_edges = train(
+        resume=resume,
+        model=model,
+        optimizer=optimizer,
+        num_epochs=num_epochs,
+        device=device,
+        train_loader=train_loader_edges,
+        test_loader=test_loader_edges,
+        latest_path=edges_latest_full,
+        best_path=edges_best_full,
+        loss_fn=loss_fn,
+        scaler=scaler,
+    )
+
+    history_gray = train(
+        resume=resume,
+        model=model,
+        optimizer=optimizer,
+        num_epochs=num_epochs,
+        device=device,
+        train_loader=train_loader_gray,
+        test_loader=test_loader_gray,
+        latest_path=gray_latest_full,
+        best_path=gray_best_full,
+        loss_fn=loss_fn,
+        scaler=scaler,
     )
 
 
